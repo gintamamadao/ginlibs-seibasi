071a2c9f628243bf0f9b0b3db605272d5e950541
gintamamadao <gintamaginsan@126.com>
2021-12-09 21:03:15 +0800

    feat

--- src/functions/delCache/index.ts
+++ src/functions/delCache/index.ts
@@ -1,16 +1,11 @@
-import { resolve, dirname, extname } from "path";
-import fsUtil from "ginlibs-file-util";
-import validateNpm from "validate-npm-package-name";
-import parser from "@babel/parser";
-import traverse from "@babel/traverse";
 import { LiftoffEnv } from "liftoff";
 import { join } from "path";
-import { sleep } from "ginlibs-utils";
 import { delRequireCache } from "delete-node-require-cache";
 
 export const delCache = async () => {
   const liftEnv: LiftoffEnv = globalThis._cliLiftEnv || {};
   const { cwd } = liftEnv;
   const file = join(cwd, "index.js");
-  delRequireCache(file);
+  const files =  delRequireCache(file);
+  console.log(files, 'files')
 };
