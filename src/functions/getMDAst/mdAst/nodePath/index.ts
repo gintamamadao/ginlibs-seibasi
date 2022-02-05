40463f86df9d73102298316c89f8b3f4f0924d0f
gintamamadao <gintamaginsan@126.com>
2022-02-06 05:04:28 +0800

    feat: md ast

---
+++ src/functions/getMDAst/mdAst/nodePath/index.ts
@@ -0,0 +1,6 @@
+export class NodePath {
+  public node: any;
+  constructor(node: any) {
+    this.node = node;
+  }
+}
--- src/functions/getMDAst/mdAst/traverse.ts
+++ src/functions/getMDAst/mdAst/traverse.ts
@@ -1,5 +1,6 @@
 import { NodeType } from "./types";
 import { isFunc } from "ginlibs-type-check";
+import { NodePath } from "./nodePath";
 
 export type Options = {
   [p in NodeType]?: (path: any) => void;
@@ -17,26 +18,38 @@ type AnyFunction = (...args: any[]) => any;
 const noop: AnyFunction = () => undefined;
 
 export const traverse = (node: any, options: Options | OptionsEE) => {
-  const type = node.type || "root";
-  const children = node.children || [];
   const opts: any = options;
   const travTypes = Object.keys(opts);
-  let enter = noop;
-  let exit = noop;
-  if (isFunc(opts)) {
-    enter = opts;
-  } else if (isFunc(opts.enter)) {
-    enter = opts.enter;
-  }
-  if (isFunc(opts.exit)) {
-    exit = opts.exit;
-  }
 
-  const checkNode = (node: any, options: Options | OptionsEE) => {
-    const type = node?.type || "root";
-    if (!node || !node.type || !travTypes.includes(type)) {
+  const travNodes = (node: any) => {
+    const children = node.children || [];
+    for (const it of children) {
+      checkNode(it);
+    }
+  };
+
+  const checkNode = (node: any) => {
+    if (!node || !node.type) {
       return;
     }
-    const children = node.children || [];
+    const type = node?.type;
+    if (!travTypes.includes(type)) {
+      return travNodes(node);
+    }
+    const handleOpts = opts[type];
+    let enter = noop;
+    let exit = noop;
+    if (isFunc(handleOpts)) {
+      enter = handleOpts;
+    } else if (isFunc(handleOpts.enter)) {
+      enter = handleOpts.enter;
+    }
+    if (isFunc(handleOpts.exit)) {
+      exit = handleOpts.exit;
+    }
+    const nPath = new NodePath(node);
+    enter(nPath);
+    travNodes(node);
+    exit(nPath);
   };
 };
